19a4a868c152b4e56a1bb752125f29d714ec7c28
Xxeiu <milysjka@gmail.com>
2025-12-27 18:59:57 +0200

    Create Student.cs
    Add derived class Student.cs with inheritance and methods

---
+++ UniversitySystem/UniversitySystem.Common/Student.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace UniversitySystem.Common
+{
+    // Похідний клас Student, який наслідує Person. Виконання вимоги про наслідування (Завдання 1)
+    public class Student : Person
+    {
+        // Властивість 1
+        public string StudentId { get; set; }
+
+        // Властивість 2
+        public int Course { get; set; } 
+
+        // Властивість 3
+        public double AverageGrade { get; set; }
+
+        // Конструктор за замовчуванням (Завдання 2)
+        public Student() : base()
+        {
+            // Логіка ініціалізації
+            StudentId = $"S-{new Random().Next(1000, 9999)}";
+        }
+
+        // Конструктор з параметрами (Завдання 2)
+        public Student(string firstName, string lastName, int course) 
+            : base(firstName, lastName) // Виклик конструктора базового класу
+        {
+            Course = course;
+        }
+
+        // Метод (Завдання 2)
+        public void Study()
+        {
+            Console.WriteLine($"{FirstName} {LastName} (Курс {Course}) активно навчається.");
+        }
+    }
+}
