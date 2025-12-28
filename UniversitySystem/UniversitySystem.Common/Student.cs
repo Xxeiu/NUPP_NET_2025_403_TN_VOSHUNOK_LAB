20dcb1bbaf03f9349a094e23a73fca507f07889b
Xxeiu <milysjka@gmail.com>
2025-12-28 18:21:50 +0200

    Update Student.cs

--- UniversitySystem/UniversitySystem.Common/Student.cs
+++ UniversitySystem/UniversitySystem.Common/Student.cs
@@ -2,33 +2,33 @@ using System;
 
 namespace UniversitySystem.Common
 {
-    // Похідний клас Student, який наслідує Person. Виконання вимоги про наслідування (Завдання 1)
+    
     public class Student : Person
     {
-        // Властивість 1
+       
         public string StudentId { get; set; }
 
-        // Властивість 2
+       
         public int Course { get; set; } 
 
-        // Властивість 3
+        
         public double AverageGrade { get; set; }
 
-        // Конструктор за замовчуванням (Завдання 2)
+       
         public Student() : base()
         {
-            // Логіка ініціалізації
+           
             StudentId = $"S-{new Random().Next(1000, 9999)}";
         }
 
-        // Конструктор з параметрами (Завдання 2)
+        
         public Student(string firstName, string lastName, int course) 
-            : base(firstName, lastName) // Виклик конструктора базового класу
+            : base(firstName, lastName) 
         {
             Course = course;
         }
 
-        // Метод (Завдання 2)
+        
         public void Study()
         {
             Console.WriteLine($"{FirstName} {LastName} (Курс {Course}) активно навчається.");
@@ -36,15 +36,14 @@ namespace UniversitySystem.Common
     }
 }
 using System;
-using System.Collections.Generic; // Якщо ще не додано
-
+using System.Collections.Generic; 
 namespace UniversitySystem.Common
 {
     public class Student : Person
     {
-        // ... (існуючі властивості та конструктори)
+       
 
-        // Статичний метод для створення нового об'єкта зі згенерованими даними (Завдання 2)
+        
         private static readonly Random Rnd = new Random();
 
         public static Student CreateNew()
@@ -56,7 +55,7 @@ namespace UniversitySystem.Common
             )
             {
                 StudentId = $"S-{Guid.NewGuid().ToString().Substring(0, 4)}",
-                AverageGrade = Math.Round(Rnd.NextDouble() * 2 + 3, 2) // Оцінка від 3.00 до 5.00
+                AverageGrade = Math.Round(Rnd.NextDouble() * 2 + 3, 2) 
             };
         }
     }
