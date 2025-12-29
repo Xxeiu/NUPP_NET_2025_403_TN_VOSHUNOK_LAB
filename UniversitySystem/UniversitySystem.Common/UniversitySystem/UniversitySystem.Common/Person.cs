f617f4d7dba5efe22fea14bb352e858e0d23459d
Xxeiu <milysjka@gmail.com>
2025-12-29 03:21:51 +0200

    Update Person.cs

--- UniversitySystem/UniversitySystem.Common/UniversitySystem/UniversitySystem.Common/Person.cs
+++ UniversitySystem/UniversitySystem.Common/UniversitySystem/UniversitySystem.Common/Person.cs
@@ -2,14 +2,14 @@ using System;
 
 namespace UniversitySystem.Common
 {
-    // Базовий клас для доменних об'єктів (Student, Professor)
+    
     public abstract class Person
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        // Конструктор
+        
         public Person(string firstName, string lastName)
         {
             FirstName = firstName;
